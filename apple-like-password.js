d4a8f809bfc234e737286209a0cd7e8e5475ac4a
Patrick Finnegan <pfinnegan.dev@outlook.com>
2019-12-25 12:45:15 -0500

    Update apple-like-password.js

--- apple-like-password.js
+++ apple-like-password.js
@@ -1,13 +1,12 @@
-var length = ( 6 * 3 ) - 2;
+var length = ( 6 * 3 ) - 2; // Sets the length to 16
 var output = '';
 
 function apple(length) {
     let chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-    
-	for (var i = 0; i <= length; i++) {
+    for (var i = 0; i <= length; i++) {
         var rnum = Math.floor(Math.random() * chars.length);
-        if(i % 6 == 0 && i/5 != 0) output += "-"
-		output += chars.substring(rnum,rnum+1);
+        if(i % 6 == 0 && i/5 != 0) output += "-"; // Chunks the password into 3 sections of 6
+	output += chars.substring(rnum,rnum+1);
     }
     console.log(output);
 }
