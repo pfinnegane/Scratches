99517cd708f9bf12e18ddd5b7e5c415808fb5be3
Patrick Finnegan <pfinnegan.dev@outlook.com>
2019-12-25 12:49:23 -0500

    Made docblocks more legible

--- abbreviate.py
+++ abbreviate.py
@@ -1,10 +1,9 @@
 def abbreviation(name):
-  """Concatenates the first letter of each word in a string preceeded by a full stop.
-  Arguments:
-  string - to be converted into an abbreviation
-  Returns:
-  string - abbreviation
+  """abbrevation(sting) => string
+  Concatenates the first letter of each word in a string preceeded by a full stop.
   Can also return a list if preferred by removing the join function.
+    @param string - to be converted into an abbreviation
+    @return string - abbreviation
   """
   return '.'.join([i.upper()[0] for i in name.split()])
 print(abbreviation("a string here"))
