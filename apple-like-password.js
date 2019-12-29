60a1ca722807064681b52e6165cf41de5cd006cb
Patrick Finnegan <pfinnegan.dev@outlook.com>
2019-12-29 10:21:09 -0500

    ✨ Updated ALP to include RegEx testingThe function will now run until a password with at least a single digit is contained within the password.

--- apple-like-password.js
+++ apple-like-password.js
@@ -1,14 +1,23 @@
-var length = ( 6 * 3 ) - 2; // Sets the length to 16
+var length = ( 6 * 3 ) - 2;
 var output = '';
+const reg = RegExp("^(?=.*\d{3})([a-zA-Z0-9]{6})\-([a-zA-Z0-9]{6})\-([a-zA-Z0-9]{6}){1}$");
 
 function apple(length) {
+    output = '';
+
     let chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+    
     for (var i = 0; i <= length; i++) {
         var rnum = Math.floor(Math.random() * chars.length);
-        if(i % 6 == 0 && i/5 != 0) output += "-"; // Chunks the password into 3 sections of 6
-	output += chars.substring(rnum,rnum+1);
+        if(i % 6 == 0 && i/5 != 0) output += "-"
+        output += chars.substring(rnum,rnum+1);
     }
-    console.log(output);
+
+    return output;
 }
 
-apple(length);
+do{
+    newPassword = apple(length);
+} while (newPassword.match(reg) == false)
+
+console.log(newPassword);
