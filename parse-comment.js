959cba226cf5d9870d80d80e1dd036c193ba3029
Patrick Finnegan <27660434+pfinnegane@users.noreply.github.com>
2020-02-26 11:11:46 -0500

    Create parse-comment.js

---
+++ parse-comment.js
@@ -0,0 +1,20 @@
+const test_string_1 = "### This is meant to be a comment ### this is out of the comment ### this is another comment ###";
+let test_string_2 = "#### This is another comment### This is out of comment ###$ This is a borked comment \n and multiline ###";
+let test_string_3 = "### This is the first comment ### ### This is another comment \n but multiline ###";
+
+function test_replaceCommentTags(test_string) {
+    const split = test_string.replace(/\r\n|\n|\r/gm, " ").split(' ');
+
+    for ( let i in split) {
+        if(split[i] == '###' && i % 2 == 0 && i != 0) {
+            split[i] = '-###';
+        } else if(split[i] == '###'){
+            split[i] = '###-';
+        }
+    }
+    return split;
+}
+
+console.log(test_replaceCommentTags(test_string_1));
+console.log(test_replaceCommentTags(test_string_2));
+console.log(test_replaceCommentTags(test_string_3));
