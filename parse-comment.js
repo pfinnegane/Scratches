4102407c437b935af8ec54f7e72f981f744a9c57
Patrick Finnegan <27660434+pfinnegane@users.noreply.github.com>
2020-02-26 11:15:18 -0500

    Update parse-comment.js

--- parse-comment.js
+++ parse-comment.js
@@ -1,9 +1,9 @@
 const test_string_1 = "### This is meant to be a comment ### this is out of the comment ### this is another comment ###";
-let test_string_2 = "#### This is another comment### This is out of comment ###$ This is a borked comment \n and multiline ###";
-let test_string_3 = "### This is the first comment ### ### This is another comment \n but multiline ###";
+const test_string_2 = "#### This is another comment### This is out of comment ###$ This is a borked comment \n and multiline ###";
+const test_string_3 = "### This is the first comment ### ### This is another comment \n but multiline ###";
 
 function test_replaceCommentTags(test_string) {
-    const split = test_string.replace(/\r\n|\n|\r/gm, " ").split(' ');
+    let split = test_string.replace(/\r\n|\n|\r/gm, " ").split(' ');
 
     for ( let i in split) {
         if(split[i] == '###' && i % 2 == 0 && i != 0) {
